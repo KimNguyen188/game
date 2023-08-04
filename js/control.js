7a75dcdd619cbad8c79b005779df63eae34d0c21
KimNguyen188 <141378260+KimNguyen188@users.noreply.github.com>
2023-08-04 11:07:48 +0700

    Delete js directory

--- js/control.js
+++
@@ -1,8 +0,0 @@
-// JavaScript Document
-var typed = new Typed('.element', {
-           strings: ["Web Designer.", "Code Developer.","Apps Designer."],
-     typeSpeed:50,
-      backSpeed:50,
-       loop:true,
-  });
-$('.counter').countUp();
